dedba9e887316d60c5f7dd7f4bb05c5bcb629b59
IronSoul10 <llaltairll@outlook.com>
2025-03-05 14:24:04 -0600

    Avance

--- Assets/Progra 3/Data_Base/PlayFabManager.cs
+++ Assets/Progra 3/Data_Base/PlayFabManager.cs
@@ -4,53 +4,105 @@ using TMPro;
 using UnityEngine;
 using System;
 using System.Collections;
+using UnityEngine.Events;
 
-//public class PlayFabManager : MonoBehaviour
-//{
-//    [Header("PlayFab Settings")]
-//    [SerializeField] private string titleId = "3020F";
-//    [SerializeField] private string secretKey = "ERSDCC7P8IQQGMYBWCB5OWT83UHCOIRTJT1YFIOAI8ABA3HNRR";
+public class PlayFabManager : MonoBehaviour
+{
+    [Header("PLAYFAB SETTINGS")]
+    [SerializeField] private string titleID = "1C3E3";
+    [SerializeField] private string secretKey = "C1K6MS4KCPD56NIEY7EI7B4H737BOO153ZFCJTD4YAO9PIXY55";
 
-//    [Header("Inputs")]
-//    [SerializeField] private TMP_InputField newUsernameInput;
-//    [SerializeField] private TMP_InputField setPasswordInput;
+    [Header("Create Account Inputs")]
+    [SerializeField] private TMP_InputField newUsernameInput;
+    [SerializeField] private TMP_InputField setPasswordInput;
 
-//    private void Start()
-//    {
-//        if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
-//        {
-//            PlayFabSettings.TitleId = titleId;
-//            PlayFabSettings.DeveloperSecretKey = secretKey;
+    [Header("Log In Inputs")]
+    [SerializeField] private TMP_InputField usernameInput;
+    [SerializeField] private TMP_InputField passwordInput;
+    [SerializeField] private UnityEvent onLogin;
 
-//        }
-//    }
-//    private void Update()
-//    {
-//        //Cursor.lockState = CursorLockMode.None;
-//    }
-//    void RegistrerUsuer()
-//    {
-//        if (string.IsNullOrEmpty(newUsernameInput.text) || string.IsNullOrEmpty(setPasswordInput.text))
-//        {
-//            Debug.LogError("Username or password is empty");
-//            return;
-//        }
+    [Header("User Info")]
+    [SerializeField] private TMP_Text userDisplayNameText;
 
-//        var request = new RegisterPlayFabUserRequest() // Crea una solicitud a Playfab para registrar un usuario nuevo 
-//        {
-//            DisplayName = "", // nombre con la que aparecerá en el juego
-//            Username = newUsernameInput.text, // registro de usuario
-//            Password = setPasswordInput.text // contraseña
-//        };
-//        //PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, PlayfabErrorMessage);
-//    }
+    private string userDisplayName;
 
-//    private void OnRegisterSuccess(RegisterPlayFabUserRequest result )
-//    {
-//        Debug.Log("User registered successfully");
-//    }
-//    private void PlayfabErrorMessage( PlayFabError error)
-//    {
-//        Debug.Log("Error registering user");
-//    }
-//}
+    private void Start()
+    {
+        if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
+        {
+            PlayFabSettings.TitleId = titleID;
+            PlayFabSettings.DeveloperSecretKey = secretKey;
+        }
+    }
+    private void Update()
+    {
+            Cursor.lockState = CursorLockMode.None;
+        
+    }
+
+    public void RegisterUser()
+    {
+        if (string.IsNullOrEmpty(PlayFabSettings.TitleId) || string.IsNullOrEmpty(PlayFabSettings.DeveloperSecretKey))
+        {
+            PlayFabSettings.TitleId = titleID;
+            PlayFabSettings.DeveloperSecretKey = secretKey;
+        }
+
+        var request = new RegisterPlayFabUserRequest()
+        {
+            DisplayName = newUsernameInput.text,
+            Username = newUsernameInput.text,
+            Password = setPasswordInput.text,
+            RequireBothUsernameAndEmail = false
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSucces, PlayfabErrorMessage);
+
+    }
+
+    private void OnRegisterSucces(RegisterPlayFabUserResult result)
+    {
+        Debug.Log("USUARIO REGISTRADO CORRECTAMENTE");
+    }
+
+    public void LogInUser()
+    {
+        if (string.IsNullOrEmpty(usernameInput.text) || string.IsNullOrEmpty(passwordInput.text))
+        {
+            Debug.LogWarning("ALGUNO DE LOS CAMPOS ESTA VACIO");
+            return;
+        }
+
+        var request = new LoginWithPlayFabRequest()
+        {
+            Username = usernameInput.text,
+            Password = passwordInput.text,
+        };
+
+        PlayFabClientAPI.LoginWithPlayFab(request, OnLoginSucces, PlayfabErrorMessage);
+    }
+
+    private void OnLoginSucces(LoginResult result)
+    {
+        Debug.Log("SESION INICIADA CORRECTAMENTE");
+    }
+
+    public void GetDisplayName()
+    {
+        var request = new GetPlayerProfileRequest();
+        PlayFabClientAPI.GetPlayerProfile(request,OnGetDisplayNameSucced,PlayfabErrorMessage);
+    }
+
+    private void OnGetDisplayNameSucced(GetPlayerProfileResult result)
+    {
+        userDisplayName = result.PlayerProfile.DisplayName;
+
+        userDisplayNameText.text = userDisplayName;
+    }
+
+    private void PlayfabErrorMessage(PlayFabError error)
+    {
+        Debug.LogWarning(error.ErrorMessage);
+    }
+
+}
